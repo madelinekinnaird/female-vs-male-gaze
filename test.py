a39f7c3676b7a89e77a3d48d96e9d4bf38cc57e1
Madeline Kinnaird <madeline.kinnaird@gmail.com>
2021-12-11 11:19:17 -0500

    plotly events

--- test.py
+++ test.py
@@ -15,19 +15,27 @@ import streamlit as st
 #https://towardsdatascience.com/read-data-from-google-sheets-into-pandas-without-the-google-sheets-api-5c468536550
 # https://stackoverflow.com/questions/61484573/how-can-one-write-to-a-publicly-available-google-sheet-without-authorization-i
 
+# https://www.youtube.com/watch?v=JeUMb0OgUGg
+"""
+# Male vs. Female Gaze!
 
 """
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+
+
 test = """
 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSegpZENRtp3OGe2_aVJmRGy7vupRpiwH4A9fqxKw3Y7KolMPA/viewform?embedded=true" width="640" height="446" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 """
+from PIL import Image
+image = Image.open('images/pete-davidson.jpg')
 
 
+
+"""
+# Pete Davidson:
+
+"""
+
+st.image(image)
 
 
 st.markdown(test, unsafe_allow_html = True)
@@ -35,21 +43,23 @@ st.markdown(test, unsafe_allow_html = True)
 ## plot for each dude
 import streamlit as st
 from streamlit_plotly_events import plotly_events
-
+#https://github.com/madelinekinnaird/female_vs_male_gaze/blob/main/gaze_background1.png?raw=true
 # Writes a component similar to st.write()
-fig = px.scatter(x=[1,2,3], y=[1,2,3]).add_layout_image(
+fig = px.scatter(x=[-2,-2,2], y=[-2,2,1]).add_layout_image(
         dict(
-            source="https://images.plot.ly/language-icons/api-home/python-logo.png",
+            source="https://raw.githubusercontent.com/madelinekinnaird/female_vs_male_gaze/main/gaze_background1.png",
             xref="x",
             yref="y",
-            x=0,
-            y=3,
-            sizex=2,
-            sizey=2,
-            sizing="stretch",
-            opacity=0.5,
+            x=-2,
+            y=2,
+            sizex=4,
+            sizey=4,
+            #sizing="stretch",
+            #opacity=0.5,
             layer="below")
 )
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
 selected_points = plotly_events(fig)
 
 
@@ -75,8 +85,8 @@ x = [200,1400,1200,1350]
 y = [550,1000,500,1250]
 
 
-img = plt.imread("gaze_background.png")
-fig, ax = plt.subplots(figsize=(9, 10))
+img = plt.imread("gaze_background1.png")
+fig, ax = plt.subplots(figsize = (10,9))
 ax.imshow(img)
 ax.scatter(x, y)
 
@@ -84,7 +94,7 @@ ax.scatter(x, y)
 for x0, y0, path in zip(x, y, paths):
     ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False)
     ax.add_artist(ab)
-
+st.write(fig)
 
 if st.button('Say hello'):
     st.write('Why hello there')
