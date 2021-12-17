9830322fe0ceadac213cd73ac4a8f7e833e4b830
Madeline Kinnaird <madeline.kinnaird@gmail.com>
2021-12-17 01:07:27 -0500

    adding port

--- mainApp.py
+++ mainApp.py
@@ -30,7 +30,7 @@ import psycopg2
 import mysql.connector
 
 def init_connection():
-    return mysql.connector.connect(host='localhost', user = "root", password = "Bdflmnptv1!", database = "gazes")
+    return mysql.connector.connect(host='localhost',port = 3306, user = "root", password = "Bdflmnptv1!", database = "gazes")
     #return psycopg2.connect(host = "localhost",port = 5432,database = "gaze_database",user = "postgres", password = os.environ['PASSWORD_KEY'])
 
 
