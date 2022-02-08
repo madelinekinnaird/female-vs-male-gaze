c78d2b1f61078a5bb8f8c198f7d352d63c9af343
Madeline Kinnaird <madeline.kinnaird@gmail.com>
2022-02-07 21:37:50 -0500

    update sql

--- mainApp.py
+++ mainApp.py
@@ -47,7 +47,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from gaze_table;")
 
 # Print results.
 for row in rows:
