bc4db5ac1ab7d37f6d08dabb6e60580c83b739eb
Madeline Kinnaird <madeline.kinnaird@gmail.com>
2022-02-07 21:38:44 -0500

    remove cache

--- mainApp.py
+++ mainApp.py
@@ -34,14 +34,14 @@ import mysql.connector
 
 # Initialize connection.
 # Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+#@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
 conn = init_connection()
 
 
-@st.cache(ttl=600)
+#@st.cache(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
